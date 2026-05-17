bb5941b22c1a27f4e3f8ec08985ed6b131ab522e
Fc_Clb <lexiangjizhi@qq.com>
2026-05-17 15:10:41 +0800

    项目骨架搭建

---
+++ api-passenger/src/main/java/com/fc/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,14 @@
+package com.fc.apipassenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPassengerApplication.class);
+        System.out.println("Api Passenger Application Started !!!");
+    }
+
+}
