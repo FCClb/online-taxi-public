14d52b9bf90a0e645a952d6385c21f1aaee76364
Lenovo <123@qq.com>
2026-05-21 09:47:57 +0800

    返回值统一处理 代码编写

---
+++ internal-common/src/main/java/com/fc/internalcommon/dto/ResponseResult.java
@@ -0,0 +1,61 @@
+package com.fc.internalcommon.dto;
+
+import com.fc.internalcommon.constant.CommonStatusEnum;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * 返回值统一
+ * @param <T>
+ */
+@Data
+@Accessors(chain = true)
+public class ResponseResult<T> {
+
+    private int code;
+    private String message;
+    private T data;
+
+    /**
+     * 成功响应的方法
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> ResponseResult success(T data) {
+
+        return new ResponseResult().setCode(CommonStatusEnum.SUCCESS.getCode()).setMessage(CommonStatusEnum.SUCCESS.getValue()).setData(data);
+    }
+
+    /**
+     * 失败：自定义失败 错误码和提示信息
+     * @param code
+     * @param message
+     * @return
+     */
+    public static ResponseResult fail(int code, String message) {
+        return new ResponseResult().setCode(code).setMessage(message);
+    }
+
+    /**
+     * 失败：统一的失败
+     * @param data
+     * @return
+     * @param <T>
+     */
+    public static <T> ResponseResult fail(T data) {
+        return new ResponseResult().setData(data);
+    }
+
+    /**
+     * 失败：自定义失败 错误码、提示信息和具体数据
+     * @param code
+     * @param message
+     * @param data
+     * @return
+     */
+    public static ResponseResult fail(int code, String message, String data) {
+        return new ResponseResult().setCode(code).setMessage(message).setData(data);
+    }
+
+}
