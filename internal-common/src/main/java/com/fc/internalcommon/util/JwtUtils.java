b3d18966274f1fe28f8f6c40c76e34ca9d3c68d1
Fc_Clb <lexiangjizhi@qq.com>
2026-06-02 16:01:50 +0800

    internal-common:jwt通用工具类token解析代码编写

--- internal-common/src/main/java/com/fc/internalcommon/util/JwtUtils.java
+++ internal-common/src/main/java/com/fc/internalcommon/util/JwtUtils.java
@@ -3,11 +3,11 @@ package com.fc.internalcommon.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Jwt工具类
@@ -17,22 +17,19 @@ public class JwtUtils {
     //盐
     private static final String SIGN = "@FCClb";
 
+    private static final String JWT_KEY = "passengerPhone";
+
     //生成token
-    public static String generateToken(Map<String, String> map) {
+    public static String generateToken(String passengerPhone) {
+        JWTCreator.Builder builder = JWT.create();
+
+        //整合JWT_KEY
+        builder.withClaim(JWT_KEY, passengerPhone);
+
         //token过期时间
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, 1); //找到一天后的时间点
         Date date = calendar.getTime();
-
-        JWTCreator.Builder builder = JWT.create();
-
-        //整合map
-        map.forEach(
-            (k, v) -> {
-                builder.withClaim(k, v);
-            }
-        );
-
         //整合过期时间
         builder.withExpiresAt(date);
 
@@ -42,16 +39,21 @@ public class JwtUtils {
         return sign;
     }
 
-
     //解析token
+    public static String parseToken(String token) {
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+        Claim claim = verify.getClaim(JWT_KEY);
+        return claim.toString();
+    }
 
     public static void main(String[] args) {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("name", "fufu");
-        map.put("age", "18");
+        String s = generateToken("15503478318");
 
-        String s = generateToken(map);
-        map.entrySet().forEach(System.out::println);
         System.out.println("生成的token：" + s);
+        System.out.println("==========");
+
+        System.out.println("解析token：" + parseToken(s));
+
+
     }
 }
