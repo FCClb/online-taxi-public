91c8450cafa608b89e87a310b267c6f9d327c865
Fc_Clb <lexiangjizhi@qq.com>
2026-06-11 18:29:15 +0800

    service-driver-user:司机用户服务创建

---
+++ base-service/service-driver-user/src/main/java/com/fc/servicedriveruser/ServiceDriverUserApplication.java
@@ -0,0 +1,12 @@
+package com.fc.servicedriveruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDriverUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDriverUserApplication.class, args);
+    }
+}
---
+++ base-service/service-driver-user/src/main/java/com/fc/servicedriveruser/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.fc.servicedriveruser.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "service-driver-user test";
+    }
+}
